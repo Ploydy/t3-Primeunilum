990263e943b91bad02bf1100e1845739d27762c2
Ploydy <Lfecarballo20@gmail.com>
2024-06-18 19:07:43 +0800

    petsupplies page added and also the products

--- primeunilum/src/app/customer/collections/pet-supplies/page.tsx
+++ primeunilum/src/app/customer/collections/pet-supplies/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import PetSuppliesCard from '~/app/ui/collections/petsuppliesCard'
+import { CardsSkeleton } from '~/app/ui/skeletons'
 
 export default function PetSupplies() {
   return (
-    <div>PetSupplies</div>
+    <div className='bg-white shadow-md'>
+      <h1 className='font-bold text-3xl pl-6 pt-4 mb-20' >
+        PetSupplies
+      </h1>
+      <ul className="grid grid-cols-1 items-start ">
+        <Suspense fallback={<CardsSkeleton />}>
+          <PetSuppliesCard />
+        </Suspense>
+      </ul>
+    </div>
   )
 }
---
+++ primeunilum/src/app/ui/collections/petsuppliesCard.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Image from "next/image";
+import Aluminum from '../../../../public/hardware/aluminumPlates.jpg'
+import { api } from '~/trpc/server';
+import Link from 'next/link';
+
+
+export default async function PetSuppliesCard() {
+
+  const Products = await api.product.getProducts();
+  const petSupplies = Products.filter(product => product.category === 'Petsupplies')
+
+  return (
+    <div>
+      {petSupplies.map((product) => (
+        <div key={product.id}>
+          <li className="relative flex flex-row items-start border-2 rounded p-2 mt-2">
+
+            <Image src={Aluminum} alt="" className="p-4 ml-10 flex-shrink-0 shadow-lg rounded w-full sm:w-[10rem] xl:mb-2" width="1280" height="200" />
+
+            <div className='flex flex-col justify-between mt-5'>
+              <div className="sm:ml-6 xl:ml-6">
+                <h3 className="mb-1 text-slate-900 font-mono">
+                  <span className="mb-1 block text-sm leading-6 text-slate-600">{product.brandname}</span><Link href={`/customer/collections/hardware/${product.name}`}>
+                    {product.name}</Link>
+                </h3>
+                <div className="prose prose-slate prose-sm text-slate-600">
+                  <p>P {product.price} PHP</p>
+                </div>
+                <p className='text-sm'>***** {product.reviews} we got reviews</p>
+              </div>
+
+            </div>
+
+            <div className='flex flex-col items-center transition-all ml-auto' >
+              <Link href='/customer/cart'>
+                <button
+                  className="shadow-md mt-5 group inline-flex items-center rounded text-md font-semibold whitespace-nowrap px-5 py-2 focus:outline-none focus:ring-2 bg-slate-200 text-slate-900 hover:bg-slate-300 focus:ring-slate-500">
+                  Add to cart
+                </button>
+              </Link>
+
+
+              <button
+                className="shadow-md my-2 group inline-flex items-center rounded text-md font-semibold whitespace-nowrap px-5 py-2 focus:outline-none focus:ring-2 bg-slate-200 text-slate-900 hover:bg-slate-300 focus:ring-slate-500">
+                Quick view
+              </button>
+            </div>
+
+          </li>
+        </div>))}
+
+    </div >
+  )
+}
