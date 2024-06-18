8798f79579c95c93839ee057020520028d73e508
Ploydy <Lfecarballo20@gmail.com>
2024-06-18 19:04:10 +0800

    housewares page added and also the products

--- primeunilum/src/app/customer/collections/housewares/page.tsx
+++ primeunilum/src/app/customer/collections/housewares/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import HousewareCard from '~/app/ui/collections/housewareCard'
+import { CardsSkeleton } from '~/app/ui/skeletons'
 
 export default function HouseWares() {
   return (
-    <div>HouseWares</div>
+    <div className='bg-white shadow-md'>
+      <h1 className='font-bold text-3xl pl-6 pt-4 mb-20' >
+        HouseWares
+      </h1>
+      <ul className="grid grid-cols-1 items-start ">
+        <Suspense fallback={<CardsSkeleton />}>
+          <HousewareCard />
+        </Suspense>
+      </ul>
+    </div>
   )
 }
