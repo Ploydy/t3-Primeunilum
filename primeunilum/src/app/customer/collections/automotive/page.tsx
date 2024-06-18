c6ce89570233a0a08b251fb721ddded393ad40bc
Ploydy <Lfecarballo20@gmail.com>
2024-06-18 18:48:32 +0800

    automotive page added and the products

--- primeunilum/src/app/customer/collections/automotive/page.tsx
+++ primeunilum/src/app/customer/collections/automotive/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import AutomotiveCard from '~/app/ui/collections/automotiveCard'
+import { CardsSkeleton } from '~/app/ui/skeletons'
 
 export default function Automotive() {
   return (
-    <div>Automotive</div>
+    <div className='bg-white shadow-md'>
+      <h1 className='font-bold text-3xl pl-6 pt-4 mb-20' >
+        Automotive
+      </h1>
+      <ul className="grid grid-cols-1 items-start ">
+        <Suspense fallback={<CardsSkeleton />}>
+          <AutomotiveCard />
+        </Suspense>
+      </ul>
+    </div>
   )
 }
