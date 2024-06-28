ea09828956b3b324f78663925b2f84e18578a7fb
Ploydy <Lfecarballo20@gmail.com>
2024-06-29 00:14:32 +0800

    admin inventory product images updated

--- primeunilum/src/app/ui/cards/products/productTable.tsx
+++ primeunilum/src/app/ui/cards/products/productTable.tsx
@@ -1,13 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+
 import React from 'react'
 import { api } from '~/trpc/server'
-import Image from 'next/image'
 import { formatPrice } from '~/app/lib/utils'
 import { DeleteProduct, UpdateProduct } from './buttons'
 
 export default async function ProductTable() {
   const products = await api.product.getProducts()
   return (
-    <section className="container mx-auto font-mono">
+    <section className=" w-full font-mono">
       <div className="w-full mb-8 overflow-x-hidden rounded-lg shadow-lg">
         <div className="w-full overflow-x-auto">
           <table className="w-full">
@@ -29,7 +30,7 @@ export default async function ProductTable() {
                   <td className="px-4 py-3 border">
                     <div className="flex items-center text-sm">
                       <div className="relative w-8 h-8 mr-3 rounded-full md:block">
-                        <img className="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
+                        <img className="object-cover w-full h-full rounded-full" src={product.img} alt="" loading="lazy" />
                         <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
                       </div>
                       <div>
